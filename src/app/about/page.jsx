ab3efd4e754727be05a736c4036cd7b72cea55f3
Yousef Bakr Zaki Alsaidi <youbakrzaki@gmail.com>
2024-06-21 06:56:45 +0300

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-step destination for
+        all your movie-related needs
+      </p>
+      <p>
+        Our website is designed to provide your with a comprehensive database of
+        movies from all around the worked, along with teh latest news, reviews,
+        and trailers. Our movie database is constantly updated with new
+        releases, ensuring{" "}
+      </p>
+    </div>
+  );
+}
