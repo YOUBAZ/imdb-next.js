98ce4bf4292ea46a0db4008d28b85a18ad086cab
Yousef Bakr Zaki Alsaidi <youbakrzaki@gmail.com>
2024-06-21 07:09:27 +0300

    create the navbar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Providers from "./Providers";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,10 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Providers>
-        <Header />
-        {children}
-      </Providers>
+        <Providers>
+          <Header />
+          <Navbar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
